f2ab5192c14ad85b75cc6ff3cca9f98e58df24bb
numq <numq.dev@gmail.com>
2023-03-31 14:07:42 +0300

    Add vertical scroll

--- desktop/src/jvmMain/kotlin/column/ImageColumn.kt
+++ desktop/src/jvmMain/kotlin/column/ImageColumn.kt
@@ -2,6 +2,10 @@ package column
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -12,12 +16,14 @@ import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import image.ImageItem
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -28,6 +34,7 @@ fun ImageColumn(
     onUpload: () -> Unit,
     itemContent: @Composable (Int, ImageItem) -> Unit,
 ) {
+    val coroutineScope = rememberCoroutineScope()
     Column(
         modifier.padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -37,7 +44,11 @@ fun ImageColumn(
             Icon(Icons.Rounded.Add, "upload")
         }
         LazyColumn(
-            Modifier.weight(1f),
+            Modifier.weight(1f).draggable(rememberDraggableState { delta ->
+                coroutineScope.launch {
+                    listState.scrollBy(-delta)
+                }
+            }, Orientation.Vertical),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(4.dp),
             state = listState
